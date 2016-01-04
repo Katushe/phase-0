85ff522e0bd37dd09344a9095eaf6c3f72c1b5b0
Katushe <kgarbacz11@gmail.com>
2016-01-03 23:53:13 -0600

    fix some answers about the errors

--- week-4/errors.rb
+++ week-4/errors.rb
@@ -26,9 +26,9 @@ end
 # 4. What additional information does the interpreter provide about this type of error?
 #The interpreter says it did not expect the end-of-input, rather it expected keyword_end
 # 5. Where is the error in the code?
-#The error is in the while loop
+#The error is at the end of the while loop
 # 6. Why did the interpreter give you this error?
-#While loops need to have their own end statement, while this only had an end statement for the method definition.
+#While loops need to have their own end statement, while this only had an end statement for the method definition. 170 is the last line of code-the reason the error gave that line is that it went through the whole document looking for that missing "end".
 
 # --- error -------------------------------------------------------
 
@@ -41,7 +41,7 @@ south_park = ''
 # 3. What additional information does the interpreter provide about this type of error?
 #It says that 'south_park' is undefined and it doesnt know if it's a local variable or a method
 # 4. Where is the error in the code?
-#The error is in main
+#The error is in 'south_park'
 # 5. Why did the interpreter give you this error?
 #Line 35 makes a call to south_park, but this has not been defined as a variable or as a method anywhere else in the file.
 
@@ -56,7 +56,7 @@ end
 # 3. What additional information does the interpreter provide about this type of error?
 #It says the method 'cartman is undefined'
 # 4. Where is the error in the code?
-#The error is in main
+#The error is in the call to cartman()
 # 5. Why did the interpreter give you this error?
 #Line 50 makes a call to the method cartman (the interpretor knows its a method because of the parentheses) but this method has not been defined anywhere in the file.
 
@@ -75,7 +75,7 @@ cartmans_phrase
 # 3. What additional information does the interpreter provide about this type of error?
 #It says there is the wrong number of arguments and '(1 for 0)' which means there's an argument where there shouldn't be one.
 # 4. Where is the error in the code?
-#The error is on line 69
+#The error is in the call to cartmans_phrase on line 69-wrong number of arguments
 # 5. Why did the interpreter give you this error?
 #The method cartmans_phrase is defined as not having any arguments but then the call to it on 69 uses an argument.
 
@@ -94,7 +94,7 @@ cartman_says('I hate Kyle')
 # 3. What additional information does the interpreter provide about this type of error?
 #It says there's the wrong number of arguments and '(0 for 1)' which means there isn't an argument where there shoukd be one.
 # 4. Where is the error in the code?
-#The error is on line 88
+#The error is in the call to cartman_says on line 88-wrong number of arguments
 # 5. Why did the interpreter give you this error?
 #The method cartman_says is defined as taking in one argument, but the call to it on line 88 does not provide an argument.
 
@@ -115,7 +115,7 @@ cartmans_lie('A meteor the size of the earth is about to hit Wyoming!', 'Kyle')
 # 3. What additional information does the interpreter provide about this type of error?
 #It says there is the wring number of arguments and (1 for 2) which means there is only 1 argument provided while there should be 2
 # 4. Where is the error in the code?
-#It is on line 109
+#It is in the call to cartmans_lie on line 109-wrong number of arguments
 # 5. Why did the interpreter give you this error?
 #The method cartmans_lie is defined as taking 2 arguments but then the call to it on line 109 only provides 1 argument.
 
@@ -172,8 +172,8 @@ amount_of_kfc_left = 20/1
 Which error was the most difficult to read?
 
   I found the first one most dificult to read, even though, because of the hint, I wasn't too thrown off by the line #
-  in the error message as I know many others were- I realized that it was going through to the end of the file looking for that 
-  missing "end" statement.Instead I was thrown off since the error said it was expecting a 'keyword_end' but the method did 
+  in the error message like some others were- I realized that it was going through to the end of the file looking for the
+  missing "end" statement.Instead I was thrown off since the error said it was expecting a 'keyword_end' but the method did
   already have an "end" statement.
 
 How did you figure out what the issue with the error was?
@@ -184,12 +184,12 @@ How did you figure out what the issue with the error was?
 
 Were you able to determine why each error message happened based on the code?
 
-  Yes, I thought most of the error messages provided enough information that I could figure out what the general problem was, 
+  Yes, I thought most of the error messages provided enough information that I could figure out what the general problem was,
   and then it was easy to look through the code and figure out the specific problem with those lines.
 
 When you encounter errors in your future code, what process will you follow to help you debug?
 
-I will read the error message first, and try to get a general understanding of what the problem might be so I know what to look 
+I will read the error message first, and try to get a general understanding of what the problem might be so I know what to look
 for in the code. Then I will go look through the code in the line/area where it told me the error is to find/fix the problem.
 
 
