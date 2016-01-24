3549cb17a2d816e269999174a192cb75bb221c61
Katushe <kgarbacz11@gmail.com>
2016-01-24 11:05:22 -0600

    remove arguments in double and sum--not needed since they are instance variables

--- week-6/credit-card/my_solution.rb
+++ week-6/credit-card/my_solution.rb
@@ -37,29 +37,29 @@ class CreditCard
 
   end
 
-  def double(number)
-    @doubled = @number.map!{|x| if @number.find_index(x).even? then x.to_i * 2 else x = x.to_i end }
+  def check_card
+    @temp = double
+    @total = sum
+   if @total % 10 == 0
+     return true
+   else
+     return false
+   end
   end
 
+  private
 
-  def sum(number)
+  def double
+      @doubled = @number.map!{|x| if @number.find_index(x).even? then x.to_i * 2 else x = x.to_i end }
+    end
+
+  def sum
     @doubled.map!{|x| x.to_s}
     @joined = @doubled.join('').split('')
     @joined.map!{|x| x.to_i}
     @total = 0
     @joined.each { |n| @total = @total + n.to_i }
     return @total
-
-  end
-
-  def check_card
-    @temp = double(@number)
-    @total = sum(@temp)
-   if @total % 10 == 0
-     return true
-   else
-     return false
-   end
   end
 
 end
@@ -71,16 +71,17 @@ class CreditCard
     @number = number.to_s.split('')
   end
 
-  def double(number)
+  def double
      @number.map!{|x| @number.find_index(x).even? ? x.to_i * 2 : x = x.to_i }
   end
 
-  def sum(number)
+  def sum
     @number.map!{|x| x.to_s}.join('').split('').map!{|x| x.to_i}.reduce(:+)
   end
 
   def check_card
-   sum(double(@number)) % 10 == 0 ? true : false
+    double
+    sum % 10 == 0 ? true : false
   end
 
 end
@@ -90,7 +91,7 @@ end
 What was the most difficult part of this challenge for you and your pair?
 
   It took us a while to realise that we should pass number as an argument to double and sum
-  and then call them from within check_card. We also had a bit of trouble with spliting up the 
+  and then call them from within check_card. We also had a bit of trouble with spliting up the
   digits for suming up, since first you have to turn them into strings, then join them, then split them again.
 
 What new methods did you find to help you when you refactored?
@@ -99,9 +100,9 @@ What new methods did you find to help you when you refactored?
 
 What concepts or learnings were you able to solidify in this challenge?
 
-  How class methods/variables work when it comes to driver code. For a while, my pair and I were explicitly calling 
-  both double and sum in our driver code, and while it seemed to be working, we were failing the tests. So the idea 
-  of calling a method within another method instead of outside of the class became clearer once we realised we should 
+  How class methods/variables work when it comes to driver code. For a while, my pair and I were explicitly calling
+  both double and sum in our driver code, and while it seemed to be working, we were failing the tests. So the idea
+  of calling a method within another method instead of outside of the class became clearer once we realised we should
   call them in check_card.
 
 =end
