07bbd903625b99677c60fe9fb29344de29ef3ef2
Katushe <kgarbacz11@gmail.com>
2016-02-04 23:07:22 -0600

    update reflection

--- week-9/ruby-review-1/ruby-review.rb
+++ week-9/ruby-review-1/ruby-review.rb
@@ -3,6 +3,17 @@
 
 
 # Pseudocode
+=begin
+DEFINE method super_fizzbuzz, which takes in 1 agrument, an array
+  For each item in the array
+    IF the item is divisible by 3 & 5
+      set the item equal to "FizzBuzz"
+    ELSIF the item is divisible by 3
+      set the item equal to "Fizz"
+    ELSIF the item is divisible by 5
+      set the item equal to "Buzz"
+    ELSE set the item equal to itself
+=end
 
 
 
@@ -39,23 +50,21 @@ end
 =begin
 What concepts did you review in this challenge?
 
-  I reviewed iterating through an array, destructively
-  modifying an array(using map), and ternary operators.
+  I reviewed iterating through an array, destructively modifying an array
+  (using map), and ternary operators.
 
 What is still confusing to you about Ruby?
 
-  At this point, I think I have a fairly good grasp of the
-  concepts we've done so far. My biggest challenge at the
-  moment is getting used to switching between languages
-  more fluidly (I still find myself making syntax errors when
-  going from Ruby <-> JS)
+  At this point, I think I have a fairly good grasp of the concepts we've done
+  so far. My biggest challenge at the moment is getting used to switching between
+  languages more fluidly (I still find myself making syntax errors when going
+  from Ruby <-> JS)
 
 What are you going to study to get more prepared for Phase 1?
 
-  I want to practice more with writing my own tests, since I
-  haven't done much of that outside of plain driver code. I
-  also would like to look in to recursion a bit, just because
-  I've hit upon it quite a few times during research in the
-  past few weeks but mostly put it on the backburner since we
-  weren't getting into it yet and I didn't want to get too off track.
+  I want to practice more with writing my own tests, since I haven't done much of
+  that outside of plain driver code. I also would like to look in to recursion a bit,
+  just because I've hit upon it quite a few times during research in the past few
+  weeks but mostly put it on the backburner since we weren't getting into it yet and
+  I didn't want to get too off track.
 =end
