3371f4758db49d94c6d295e5643d66673663e7b6
Katushe <kgarbacz11@gmail.com>
2016-02-04 22:02:51 -0600

    format reflection to wrap text

--- week-9/review.js
+++ week-9/review.js
@@ -59,13 +59,20 @@ groceries.displayItems();
 
 What concepts did you solidify in working on this challenge? (reviewing the passing of information, objects, constructors, etc.)
 
-  It gave me more practice with defining/using constructors, and this was the first time I used a prototype in conjunction with a constructor to add methods to an object, so I learned something new as well.
+  It gave me more practice with defining/using constructors, and this was the first time I used a prototype in conjunction with 
+  a constructor to add methods to an object, so I learned something new as well.
 
 What was the most difficult part of this challenge?
 
-  Trying to figure out how to initialize a new grocery list with items without making each item a separate argument in the constructor, since it would be hard to write the constructor in a way that would work for any number of arguments. I quickly thought of passing in an array but them it took me a while to think through how to iterate through that array and use its contents along with "this" in the right way. And then I later looked at the old Ruby challenge and saw that the input there was a string of space-separated items so I changed it to resemble that, to make it easier on the "user", even though I'm still turning it into an array as an intermidiary to make it into an object.
+  Trying to figure out how to initialize a new grocery list with items without making each item a separate argument in the 
+  constructor, since it would be hard to write the constructor in a way that would work for any number of arguments. 
+  I quickly thought of passing in an array but them it took me a while to think through how to iterate through that array and
+  use its contents along with "this" in the right way. And then I later looked at the old Ruby challenge and saw that the input 
+  there was a string of space-separated items so I changed it to resemble that, to make it easier on the "user", even though I'm 
+  still turning it into an array as an intermidiary to make it into an object.
 
 Did an array or object make more sense to use and why?
 
-  I did use an array as an intermediary when creating the list, but it made more sense to have the actual list be an object because then you can store the item as a property and its quantity as the value.
+  I did use an array as an intermediary when creating the list, but it made more sense to have the actual list be an 
+  object because then you can store the item as a property and its quantity as the value.
 */
