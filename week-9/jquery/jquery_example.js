d567e4e138248e5773e190ec7642d4a5ce05c6f8
Katushe <kgarbacz11@gmail.com>
2016-02-05 22:54:18 -0600

    play with jquery

--- week-9/jquery/jquery_example.js
+++ week-9/jquery/jquery_example.js
@@ -17,15 +17,27 @@ $('body').css({'background-color': 'pink'})
 
 //RELEASE 2:
   //Add code here to select elements of the DOM
+  bodyElement = $('body')
+  h1Element = $('h1')
+  mascotClass = $('.mascot')
 
 
 //RELEASE 3:
   // Add code here to modify the css and html of DOM elements
+  $('body > h1')
+    .css({"color": "#80007f",
+          "border": "dashed 2px #ff0080"})
+    .on("mouseover", function(){
+      $(this).css({"visibility": "hidden"})
+    })
 
-
+    $('.mascot > h1').html("Fiddler Crabs")
 //RELEASE 4: Event Listener
   // Add the code for the event listener here
-
+  $('img').on("mouseover", function(e){
+      e.preventDefault()
+      $(this).attr("src", "fiddler-crab.jpg")
+  })
 
 //RELEASE 5: Experiment on your own
 
